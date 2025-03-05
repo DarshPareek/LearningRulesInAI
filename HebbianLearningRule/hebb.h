84a2ee9b1d54eb8849d43338ea9aaa761abd31f9
Darsh Pareek <pareekdarsh0162@gmail.com>
2025-03-06 00:23:22 +0530

    working hebbian rule of and, or gates

---
+++ HebbianLearningRule/hebb.h
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+int dot(std::vector<int> arr1, int *arr2, int rows, int columns) {
+  int i = 0, res = 0;
+  for (i = 0; i < columns - 1; i++) {
+    res += arr1[i] * arr2[i];
+  }
+  return res;
+}
+
+class Perceptron {
+public:
+  int **dataset, rows, columns, output, bias;
+  std::vector<int> input_layer;
+  Perceptron(int **_dataset, int _rows, int _columns) {
+    dataset = _dataset;
+    rows = _rows;
+    columns = _columns;
+    bias = 0;
+    std::vector<int> _input_layer(columns - 1);
+    input_layer = _input_layer;
+    fill(input_layer.begin(), input_layer.end(), 0);
+    int output = 0;
+    // Training
+    for (int i = 0; i < rows; i++) {
+      std::cout << "Weights: ";
+      for (int j = 0; j < columns - 1; j++) {
+        input_layer[j] += dataset[i][j] * dataset[i][columns - 1];
+        std::cout << input_layer[j] << " ";
+      }
+      std::cout << std::endl;
+      bias += dataset[i][columns - 1];
+    }
+  };
+  void printWeights() {
+    std::cout << "Weights: ";
+    for (int i = 0; i < columns - 1; i++) {
+      std::cout << input_layer[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Bias: " << bias << std::endl;
+  }
+  void predict(int *input) {
+    int pred = dot(input_layer, input, rows, columns);
+    pred += bias;
+    if (pred > 0)
+      std::cout << "Prediction: " << 1 << std::endl;
+    else
+      std::cout << "Prediction: " << -1 << std::endl;
+  }
+};
---
+++ HebbianLearningRule/main.cpp
@@ -0,0 +1,23 @@
+#include "hebb.h"
+int main() {
+  int data[4][3] = {
+      {1, 1, 1}, {1, -1, -1}, {-1, 1, -1}, {-1, -1, -1}}; // Basic AND gate
+  int input_layer[] = {0, 0}, input_size = 2, output_layer[] = {0},
+      output_size = 1, bias = 0;
+  int dataset_rows = 4, dataset_cols = 3,
+      i = 0; // Hebbian Rule states that neurons that wire together fire
+  int **dataset = new int *[dataset_rows];
+  for (i = 0; i < dataset_rows; i++) {
+    dataset[i] = new int[dataset_cols];
+  }
+  for (int i = 0; i < dataset_rows; i++) {
+    for (int j = 0; j < dataset_cols; j++) {
+      dataset[i][j] = data[i][j];
+    }
+  }
+  Perceptron model = Perceptron(dataset, 4, 3);
+  model.printWeights();
+  int arr[2] = {1, -1};
+  model.predict(arr);
+  return 0;
+}
